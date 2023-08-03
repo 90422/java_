7836a32ef85121057fdcb76ea5d54dd271c21898
90422 <141306568+90422@users.noreply.github.com>
2023-08-03 12:33:54 +0530

    Add files via upload

---
+++ b.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class subtraction
+{
+  public static void main (String[]args)
+  {
+    System.out.println ("subtraction of two numbers");
+    int a = 10;
+    int b = 2;
+    int c = a - b;
+      System.out.println ("result is " + c);
+  }
+}
